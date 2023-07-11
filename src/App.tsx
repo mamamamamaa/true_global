403afe8ed056bff424f9b6b325f17487c2f3b4cf
mamamama <maks.denishchuk@gmail.com>
2023-07-11 12:41:41 +0300

    add lazy page loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import { Route, Routes } from "react-router-dom";
 import { Layout } from "./components/Layout.tsx";
+import { lazy } from "react";
+
+const HomePage = lazy(() => import("./pages/Home.tsx"));
+const CategoryPage = lazy(() => import("./pages/Category.tsx"));
+const TaskPage = lazy(() => import("./pages/Task.tsx"));
+const LoginPage = lazy(() => import("./pages/Login.tsx"));
+const RegisterPage = lazy(() => import("./pages/Register.tsx"));
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<>Home page</>} />
-        <Route path="1" element={<>1 page</>} />
-        <Route path="2" element={<>2 page</>} />
+        <Route index element={<HomePage />} />
+        <Route path="/category" element={<CategoryPage />}>
+          <Route path="/:categoryId" element={<TaskPage />} />
+        </Route>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Route>
     </Routes>
   );

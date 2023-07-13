19c57fffb6ff756cddfa1bfde1939146e6aaea05
mamamama <maks.denishchuk@gmail.com>
2023-07-13 16:04:12 +0300

    add task extra reducers

--- src/redux/task/slice.ts
+++ src/redux/task/slice.ts
@@ -1,5 +1,6 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, isAnyOf, PayloadAction } from "@reduxjs/toolkit";
 import { TaskState } from "../../types/task.ts";
+import { createTask, getTasks, removeTask, updateTask } from "./operations.ts";
 
 const initialState: TaskState = {
   error: null,
@@ -7,6 +8,8 @@ const initialState: TaskState = {
   currentCategory: null,
   tasks: [],
 };
+
+const extraTaskActions = [getTasks, createTask, updateTask, removeTask];
 
 export const taskSlice = createSlice({
   name: "task",
@@ -16,7 +19,42 @@ export const taskSlice = createSlice({
       state.currentCategory = payload;
     },
   },
-  extraReducers: (builder) => builder,
+  extraReducers: (builder) =>
+    builder
+      .addCase(getTasks.fulfilled, (state, { payload }) => {
+        state.tasks = payload;
+      })
+      .addCase(createTask.fulfilled, (state, { payload }) => {
+        state.tasks.unshift(payload);
+      })
+      .addCase(updateTask.fulfilled, (state, { payload }) => {
+        state.tasks = state.tasks.map((task) =>
+          task.id === payload.id ? payload : task
+        );
+      })
+      .addCase(removeTask.fulfilled, (state, { payload }) => {
+        state.tasks = state.tasks.filter(({ id }) => id !== payload.id);
+      })
+      .addMatcher(
+        isAnyOf(...extraTaskActions.map((action) => action.pending)),
+        (state) => {
+          state.isLoading = true;
+          state.error = null;
+        }
+      )
+      .addMatcher(
+        isAnyOf(...extraTaskActions.map((action) => action.fulfilled)),
+        (state) => {
+          state.isLoading = false;
+        }
+      )
+      .addMatcher(
+        isAnyOf(...extraTaskActions.map((action) => action.rejected)),
+        (state, { payload }) => {
+          state.isLoading = false;
+          state.error = payload;
+        }
+      ),
 });
 
 export const { setCurrentCategory } = taskSlice.actions;
